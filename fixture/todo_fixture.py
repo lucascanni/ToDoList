d7933617c7d61570f0bfa432009c814c5e529f73
lucascanni <77814636+lucascanni@users.noreply.github.com>
2024-05-07 16:18:12 +0200

    add todo fixture

---
+++ fixture/todo_fixture.py
@@ -0,0 +1,17 @@
+import pytest
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+@pytest.fixture
+def create_task():
+    # Test creating a task
+    response = client.post("/todo", json={
+        "name":"Complete project proposal",
+        "description":"Write and finalize project proposal document",
+        "date":"2024-05-10",
+        "priority":"High",
+        "status":"In progress"
+    })
