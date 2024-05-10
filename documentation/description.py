ad09bd3c9bc1ecaab99ee5ae1a246c9586a55b99
lucascanni <77814636+lucascanni@users.noreply.github.com>
2024-05-10 09:39:28 +0200

    update description

--- documentation/description.py
+++ documentation/description.py
@@ -8,4 +8,13 @@ Points de terminaison:
 
 - `/todos` : Lister toutes les tâches et ajouter une nouvelle tâche.
 - `/todos/{id}` : Récupère, mets à jour ou supprime une tâche spécifique.
+
+Modèle de données:
+- `id` : Identifiant unique de la tâche.    
+- `name` : Nom de la tâche. 
+- `description` : Description de la tâche.
+- `date` : Date de la tâche.
+- `priority` : Priorité de la tâche.
+- `status` : Statut de la tâche (En cours, Terminé, En attente).
+
 """
