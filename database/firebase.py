b37fd2203408ace83babc660b7badbcdd05cb611
lucascanni <77814636+lucascanni@users.noreply.github.com>
2024-05-17 16:05:16 +0200

    update docker

--- database/firebase.py
+++ database/firebase.py
@@ -1,15 +1,17 @@
 import firebase_admin, pyrebase
 from firebase_admin import credentials
-from dotenv import load_dotenv
+from dotenv import dotenv_values, load_dotenv
 import os
 import json
 
 load_dotenv()
 
-config={
-    "FIREBASE_SERVICE_ACCOUNT_KEY": os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY"),
-    "FIREBASE_CONFIG": os.getenv("FIREBASE_CONFIG")
-}
+# config={
+#     "FIREBASE_SERVICE_ACCOUNT_KEY": os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY"),
+#     "FIREBASE_CONFIG": os.getenv("FIREBASE_CONFIG")
+# }
+
+config = dotenv_values("docker-env")
 
 cred = firebase_admin.credentials.Certificate(json.loads(config['FIREBASE_SERVICE_ACCOUNT_KEY']))
 firebase_admin.initialize_app(cred)
